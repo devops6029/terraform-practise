9559c19a8ea7dda09f4a6ad02c4b7822896c0c16
Pratap Dodda <pradodda@in.ibm.com>
2025-09-19 12:19:39 +0530

    adding NSG config

--- vm.tf
+++ vm.tf
@@ -29,6 +29,25 @@ resource "azurerm_public_ip" "my_terraform_public_ip" {
   allocation_method   = "Static"
 }
 
+# Create Network Security Group and rule
+resource "azurerm_network_security_group" "my_terraform_nsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
   location            = azurerm_resource_group.example.location
@@ -40,6 +59,12 @@ resource "azurerm_network_interface" "main" {
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = azurerm_public_ip.my_terraform_public_ip.id
   }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.main.id
+  network_security_group_id = azurerm_network_security_group.my_terraform_nsg.id
 }
 
 resource "azurerm_virtual_machine" "main" {
